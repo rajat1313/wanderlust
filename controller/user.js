a517a2e7bcd969817c69dd95b23ead18be6a6ec3
rajat1313 <18rajat1313@gmail.com>
2024-09-07 21:41:30 +0530

    add in user file

--- controller/user.js
+++ controller/user.js
@@ -1,37 +1,38 @@
-const User = require ("../Models/user");
+const User = require("../Models/user");
 
-module.exports.renderSignUpForm = (req ,res)=>{
+module.exports.renderSignUpForm = (req, res) => {
     res.render("user/signup.ejs");
 };
 
 
-module.exports.SignUp = async(req,res)=>{
-    let { username ,password ,email} = req.body;
-      const newUser = new User({email ,username});
-      const registerdUser =await User.register(newUser , password);
-      console.log(registerdUser);
-      req.login(registerdUser ,(err)=>{
-        if(err){
+module.exports.SignUp = async (req, res) => {
+    let { username, password, email } = req.body;
+    const newUser = new User({ email, username });
+    const registerdUser = await User.register(newUser, password);
+    console.log(registerdUser);
+    
+    req.login(registerdUser, (err) => {
+        if (err) {
             return next(err);
         }
-        req.flash("success" , "user was registerd");
+        req.flash("success", "user was registerd");
         res.redirect("/listings");
-      })
+    })
 
 };
 
 
-module.exports.login = (req,res)=>{
+module.exports.login = (req, res) => {
     res.render("user/login.ejs");
 };
 
 
- module.exports.logout =(req,res)=>{
-    req.logout((err)=>{
-        if (err){
+module.exports.logout = (req, res) => {
+    req.logout((err) => {
+        if (err) {
             return next(err);
         }
-        req.flash("success" , "You are logged out now!");
+        req.flash("success", "You are logged out now!");
         res.redirect("/listings");
 
     });
